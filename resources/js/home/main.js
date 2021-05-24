aa40fa542422fb34c911222372771d0481ba1381
Felipe Silva <felipe.dacs@gmail.com>
2021-05-24 15:07:28 -0300

    :100: remove comentários da criação de vueapp

--- resources/js/home/main.js
+++ resources/js/home/main.js
@@ -6,20 +6,3 @@ new Vue({
   // router,
   render: (h) => h(App),
 }).$mount("#app");
-/*
-import Vue from 'vue'
-import VueCompositionAPI, { createApp } from '@vue/composition-api'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-Vue.use(VueCompositionAPI)
-
-const app = createApp({
-  render: (h) => h(App),
-})
-
-app.mount('#app')
-
-window.Vue = Vue;
-*/
