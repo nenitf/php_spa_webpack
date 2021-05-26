9919bf90b966d15607d4b5824a7c2ebb0239e72b
Felipe Silva <felipe.dacs@gmail.com>
2021-05-26 13:31:13 -0300

    :new: add definição de modo e devtools

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,41 @@ const { VueLoaderPlugin } = require("vue-loader");
 const PhpManifestPlugin = require('webpack-php-manifest');
 const path = require("path");
 
-module.exports = {
-  entry: {
-    profissionaisApp: "./resources/js/profissionais/main.js",
-    homeApp: "./resources/js/home/main.js",
-  },
-  output: {
-    path: path.resolve(__dirname, "public", "bundles"),
-    filename: '[name].[chunkhash].js',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+module.exports = (env, options) => {
+  const isDevelopment = options.mode !== 'production'
+  return {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    entry: {
+      profissionaisApp: "./resources/js/profissionais/main.js",
+      homeApp: "./resources/js/home/main.js",
+    },
+    output: {
+      path: path.resolve(__dirname, "public", "bundles"),
+      filename: '[name].[chunkhash].js',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
         },
-      },
-      {
-        test: /\.vue$/,
-        loader: "vue-loader",
-      },
+        {
+          test: /\.vue$/,
+          loader: "vue-loader",
+        },
+      ],
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new PhpManifestPlugin({
+        pathPrefix: "/public/bundles/"
+      }),
     ],
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new PhpManifestPlugin({
-      pathPrefix: "/public/bundles/"
-    }),
-  ],
-  stats: 'minimal',
+    stats: 'minimal',
+  }
 };
